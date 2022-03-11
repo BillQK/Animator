8602545ca7fe1d4b41fd35b5e4b0dfb95160664a
Khanh Nguyen <khanhnguyen@Khanhs-MacBook-Pro.local>
2022-03-11 17:42:52 -0500

    First Commit

---
+++ src/AnimatorModel.java
@@ -0,0 +1,29 @@
+/**
+ *
+ */
+public interface AnimatorModel extends AnimatorModelState {
+  /**
+   * Decide when the object to appear.
+   */
+  public void appears();
+
+  /**
+   * Create the object.
+   */
+  public void create();
+
+  /**
+   * Move the object.
+   */
+  public void move();
+
+  /**
+   * Mutate the color/shape for the object.
+   */
+  public void changes();
+
+  /**
+   * Decide when the object to disappear.
+   */
+  public void disappear();
+}
---
+++ src/AnimatorModelState.java
@@ -0,0 +1,44 @@
+/**
+ *
+ */
+public interface AnimatorModelState {
+  /**
+   * Get the position of the pbject.
+   */
+  public void getPosition();
+
+  /**
+   * Get the width of the object.
+   */
+  public void getWidth();
+
+  /**
+   * Get the height of the object.
+   */
+  public void getHeight();
+
+  /**
+   * Get the radius of the object.
+   */
+  public void getRadius();
+
+  /**
+   * Get the tick at that moment.
+   */
+  public void getTick();
+
+  /**
+   * Get the color of the object.
+   */
+  public void getColor();
+
+  /**
+   * Get the time of the animation at that moment.
+   */
+  public void getTime();
+
+  /**
+   * Get the shape of th
+   */
+  public void getShape();
+}
