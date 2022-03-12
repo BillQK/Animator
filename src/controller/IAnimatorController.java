48f20851cd2212b83be4a29aa759ac0a2be86476
Khanh Nguyen <khanhnguyen@Khanhs-MacBook-Pro.local>
2022-03-11 23:20:37 -0500

    Adding javadocs to view and controller

--- src/controller/IAnimatorController.java
+++ src/controller/IAnimatorController.java
@@ -1,4 +1,9 @@
 package controller;
 
+/**
+ * Interface for the Animator controller. An implementation will work with the IAnimatorModel
+ * interface to provide a video of animation.
+ */
 public interface IAnimatorController {
+
 }
--- src/view/IAnimatorView.java
+++ src/view/IAnimatorView.java
@@ -1,4 +1,10 @@
 package view;
 
+/**
+ * Represents a way of presenting the animation in a form useful to end users, i.e.,
+ * a human-readable form.
+ */
 public interface IAnimatorView {
+
+  String toString();
 }
