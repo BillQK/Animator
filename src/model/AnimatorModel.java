4f75cb104e30fcd36103dc16dd9ee4e19e28417e
Khanh Nguyen <khanhnguyen@Khanhs-MacBook-Pro.local>
2022-03-11 22:53:08 -0500

    Add the main class

---
+++ src/model/AnimatorModel.java
@@ -0,0 +1,109 @@
+package model;
+
+public class AnimatorModel implements IAnimatorModel {
+  /**
+   * Decide when the object to appear.
+   */
+  @Override
+  public void appears() {
+
+  }
+
+  /**
+   * Create the object.
+   */
+  @Override
+  public void create() {
+
+  }
+
+  /**
+   * Move the object.
+   */
+  @Override
+  public void move() {
+
+  }
+
+  /**
+   * Mutate the color/shape for the object.
+   */
+  @Override
+  public void changes() {
+
+  }
+
+  /**
+   * Decide when the object to disappear.
+   */
+  @Override
+  public void disappear() {
+
+  }
+
+
+  //------------------------------ANIMATOR MODEL STATE METHOD--------------------------------//
+  /**
+   * Get the position of the pbject.
+   */
+  @Override
+  public void getPosition() {
+
+  }
+
+  /**
+   * Get the width of the object.
+   */
+  @Override
+  public void getWidth() {
+
+  }
+
+  /**
+   * Get the height of the object.
+   */
+  @Override
+  public void getHeight() {
+
+  }
+
+  /**
+   * Get the radius of the object.
+   */
+  @Override
+  public void getRadius() {
+
+  }
+
+  /**
+   * Get the tick at that moment.
+   */
+  @Override
+  public void getTick() {
+
+  }
+
+  /**
+   * Get the color of the object.
+   */
+  @Override
+  public void getColor() {
+
+  }
+
+  /**
+   * Get the time of the animation at that moment.
+   */
+  @Override
+  public void getTime() {
+
+  }
+
+  /**
+   * Get the shape of the object.
+   */
+  @Override
+  public void getShape() {
+
+  }
+}
