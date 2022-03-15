1e093733ad20c166dfed45f4750cb7f8d8330f16
BillQK <billngo47@gmail.com>
2022-03-15 19:12:44 -0400

    Rectangle & Triangle constructor fix

--- src/model/Rectangle.java
+++ src/model/Rectangle.java
@@ -4,8 +4,8 @@ import java.awt.*;
 
 public class Rectangle extends AShape {
 
-  Rectangle(String type, Color col, int posX, int posY) {
-    super(type, col, posX, posY);
+  Rectangle(String type, Color col, int posX, int posY, int width, int height) {
+    super(type, col, posX, posY, width, height);
   }
 
 }
--- src/model/Triangle.java
+++ src/model/Triangle.java
@@ -4,8 +4,8 @@ import java.awt.*;
 
 public class Triangle extends AShape {
 
-  Triangle(String type, Color col, int posX, int posY) {
-    super(type, col, posX, posY);
+  Triangle(String type, Color col, int posX, int posY, int width, int height) {
+    super(type, col, posX, posY, width, height);
   }
 
 }
