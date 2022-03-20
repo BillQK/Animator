57f414e398463bad88ca3d64d6e505a9c4f38380
Khanh Nguyen <khanhnguyen@Khanhs-MacBook-Pro.local>
2022-03-20 18:07:16 -0400

    Fixing up error in animation class

--- src/model/animation/Move.java
+++ src/model/animation/Move.java
@@ -6,13 +6,18 @@ import model.shape.AShape;
 import model.utils.Time;
 
 public class Move extends AbstractAnimation {
-  public Move(AShape shape, AnimationType type, int startTime, int endTime,
-              int endW, int endH, int endX, int endY, Color endColor) {
-    super(shape, type, startTime, endTime, endW, endH, endX, endY, endColor);
+
+  public Move(AShape shape, AnimationType type, int startTime, int endTime, Color endColor) {
+    super(shape, type, startTime, endTime, endColor);
   }
 
   @Override
   public void animate(Time time) {
 
+  }
+
+  @Override
+  public String getEndsState() {
+    return null;
   }
 }
