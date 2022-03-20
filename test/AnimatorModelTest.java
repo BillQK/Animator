d5e41c611deacd7c0a51f77905b47316c766193f
Khanh Nguyen <khanhnguyen@Khanhs-MacBook-Pro.local>
2022-03-19 22:54:54 -0400

    Fix any error

--- test/AnimatorModelTest.java
+++ test/AnimatorModelTest.java
@@ -7,7 +7,9 @@ public class AnimatorModelTest {
 
   @Test
   public void testBuilder() {
-    SimpleAnimatorModel s = new SimpleAnimatorModel.AMBuilder().addRectangle("1,", 1, 3, 4, 4, 5, 5, 5, new Time(1, 2)).build();
+    SimpleAnimatorModel s = new SimpleAnimatorModel.AMBuilder().
+            addRectangle("1,", 1, 3, 4, 4, 5, 5, 5, new Time(1, 2)).build();
+
   }
 
 }
