e397a976cf29fddbf68cdb8adda22440f2b760b8
BillQK <billngo47@gmail.com>
2022-03-20 18:05:07 -0400

    Adjusted Abstract class

--- src/model/animation/AbstractAnimation.java
+++ src/model/animation/AbstractAnimation.java
@@ -11,40 +11,26 @@ public abstract class AbstractAnimation implements IAnimations {
   private final AnimationType type;
   private final int startTime;
   private final int endTime;
-  private final int endW;
-  private final int endH;
-  private final int endX;
-  private final int endY;
-  private final Color endColor;
   private AShape shape;
 
   public AbstractAnimation(AShape shape,
                            AnimationType type,
                            int startTime,
                            int endTime,
-                           int endW,
-                           int endH,
-                           int endX,
-                           int endY,
                            Color endColor) {
-    ArgumentsCheck.lessThanZero(endH, endW, endX, endY, startTime, endTime);
+    ArgumentsCheck.lessThanZero(startTime, endTime);
     if (endColor == null || type == null || shape == null) {
       throw new IllegalArgumentException("Invalid Arguments: Cannot be null");
     }
     this.shape = shape;
     this.type = type;
+
     if (shape.getTime().getStartTime() < startTime || shape.getTime().getEndTime() > endTime) {
       this.startTime = startTime;
       this.endTime = endTime;
     } else {
       throw new IllegalArgumentException("Time out of AShape Range");
     }
-
-    this.endW = endW;
-    this.endH = endH;
-    this.endX = endX;
-    this.endY = endY;
-    this.endColor = endColor;
   }
 
   @Override
@@ -61,14 +47,7 @@ public abstract class AbstractAnimation implements IAnimations {
   }
 
   @Override
-  public String getEndsState() {
-    return shape.getTime().getEndTime()
-            + shape.getName()
-            + new Posn(endX, endY)
-            + endH
-            + endW
-            + endColor.toString();
-  }
+  public abstract String getEndsState();
 
   @Override
   public String getState() {
