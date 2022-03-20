61c99156f7879532ccb9b800b46e08498390fbb6
BillQK <billngo47@gmail.com>
2022-03-20 18:07:39 -0400

    add fields for move

--- src/model/animation/Move.java
+++ src/model/animation/Move.java
@@ -3,9 +3,14 @@ package model.animation;
 import java.awt.*;
 
 import model.shape.AShape;
+import model.utils.Posn;
 import model.utils.Time;
 
 public class Move extends AbstractAnimation {
+  private final Posn original;
+  private final Posn destination;
+
+
   public Move(AShape shape, AnimationType type, int startTime, int endTime,
               int endW, int endH, int endX, int endY, Color endColor) {
     super(shape, type, startTime, endTime, endW, endH, endX, endY, endColor);
