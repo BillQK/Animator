b2b96ec2c291e45bb26e04b0b423d2ebf067ca13
BillQK <billngo47@gmail.com>
2022-03-21 17:52:30 -0400

    Finish overllappingTime

--- src/model/utils/ArgumentsCheck.java
+++ src/model/utils/ArgumentsCheck.java
@@ -17,31 +17,17 @@ public class ArgumentsCheck {
     }
   }
 
-  public static void withinShapeTime(double shapeStart, double shapeEnd,
-                                     double comStart, double comEnd) {
+  public static void withinShapeTime(double shapeStart, double shapeEnd, double comStart, double comEnd) {
     if (comStart < shapeStart || comEnd > shapeEnd) {
       throw new IllegalArgumentException("Time does not range within the bigger time frame.");
     }
   }
 
-  // 30 - 40 (0 - 50)
-  // a - b
 
-  //3 - 20
-  //1 - 17
+  public static void overlappingTime(double shapeStart, double shapeEnd, double comStart, double comEnd) {
+    if (shapeStart <= comEnd && shapeEnd >= comEnd) {
+      throw new IllegalArgumentException("Time is overlapping with another animation.");
+    }
 
-
-
-  public static void overlappingTime(double shapeStart, double shapeEnd,
-                                     double comStart, double comEnd) {
-//    if ((comEnd > shapeStart && comStart <= comEnd) || (comStart < shapeEnd && comStart <= comEnd)) {
-//      throw new IllegalArgumentException("Time is overlapping with another animation.");
-//    }
-
-//    if (!(shapeStart >= comStart && shapeEnd <= comEnd)) {
-//      throw new IllegalArgumentException("Time is overlapping with another animation.");
-//    }
-
-    if ()
   }
 }
