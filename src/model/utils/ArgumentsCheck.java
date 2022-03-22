753cc72c169fa0c56f2fb096cb3a33fa6f286a2d
Khanh Nguyen <khanhnguyen@Khanhs-MacBook-Pro.local>
2022-03-22 10:50:44 -0400

    Fix the conditions

--- src/model/utils/ArgumentsCheck.java
+++ src/model/utils/ArgumentsCheck.java
@@ -19,7 +19,8 @@ public class ArgumentsCheck {
 
   public static void withinShapeTime(double shapeStart, double shapeEnd,
                                      double comStart, double comEnd) {
-    if (comStart < shapeStart || comEnd > shapeEnd) {
+    if (comStart < shapeStart || comEnd > shapeEnd
+            || comStart > comEnd || shapeStart > shapeEnd) {
       throw new IllegalArgumentException("Time does not range within the bigger time frame.");
     }
   }
@@ -27,7 +28,8 @@ public class ArgumentsCheck {
 
   public static void overlappingTime(double shapeStart, double shapeEnd,
                                      double comStart, double comEnd) {
-    if ((shapeStart < comEnd && shapeEnd > comEnd) || (comStart > comEnd)) {
+    if ((shapeStart < comEnd && shapeEnd > comEnd)
+            || (comStart > comEnd) || (shapeStart > shapeEnd)) {
       throw new IllegalArgumentException("Time is overlapping with another animation.");
     }
 
