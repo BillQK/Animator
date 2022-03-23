679a60c76091fa7f2bf91322415ce954ec59e062
Khanh Nguyen <khanhnguyen@Khanhs-MacBook-Pro.local>
2022-03-22 23:34:30 -0400

    Check

--- src/model/utils/ArgumentsCheck.java
+++ src/model/utils/ArgumentsCheck.java
@@ -4,6 +4,7 @@ public class ArgumentsCheck {
 
   /**
    * Check if the inputs is less than zero.
+   *
    * @param args the multiple input (double) put in to check
    */
   public static void lessThanZero(double... args) {
@@ -16,6 +17,7 @@ public class ArgumentsCheck {
 
   /**
    * Check if the inputs is an empty String
+   *
    * @param args the multiple input (String) put in to check
    */
   public static void emptyString(String... args) {
@@ -26,25 +28,18 @@ public class ArgumentsCheck {
     }
   }
 
-<<<<<<< HEAD
+
   /**
    * Check if one given start and end time is within
    * the range of another given start and end time.
    *
    * @param shapeStart the range start time
-   * @param shapeEnd the range end time
-   * @param comStart the second start time to see if it is in the range time
-   * @param comEnd the second end time to see if it is in the range time
+   * @param shapeEnd   the range end time
+   * @param comStart   the second start time to see if it is in the range time
+   * @param comEnd     the second end time to see if it is in the range time
    */
-  public static void withinShapeTime(double shapeStart, double shapeEnd,
-                                     double comStart, double comEnd) {
-    if (comStart < shapeStart || comEnd > shapeEnd
-            || comStart > comEnd || shapeStart > shapeEnd) {
-=======
   public static void withinIntervalTime(double shapeStart, double shapeEnd, double comStart, double comEnd) {
-
     if (comStart < shapeStart || comEnd > shapeEnd || comStart > comEnd || shapeStart > shapeEnd) {
->>>>>>> a3e5f125a30fef7e6c34696944991dc84e850f1a
       throw new IllegalArgumentException("Time does not range within the bigger time frame.");
     }
   }
@@ -54,11 +49,11 @@ public class ArgumentsCheck {
    * another start and end time.
    *
    * @param shapeStart the first start time
-   * @param shapeEnd the first end time
-   * @param comStart the second start time to see
-   *                 if it's overlap or in the range of the first time
-   * @param comEnd the second end time to see
-   *               if it's overlap or in the range of the first time
+   * @param shapeEnd   the first end time
+   * @param comStart   the second start time to see
+   *                   if it's overlap or in the range of the first time
+   * @param comEnd     the second end time to see
+   *                   if it's overlap or in the range of the first time
    */
   public static void overlappingTime(double shapeStart, double shapeEnd,
                                      double comStart, double comEnd) {
