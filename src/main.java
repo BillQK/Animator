4eea7b918fb225656d3844b21ea911e6dca4cb1e
Khanh Nguyen <khanhnguyen@Khanhs-MacBook-Pro.local>
2022-04-03 03:34:56 -0400

    Change

--- src/main.java
+++ src/main.java
@@ -11,36 +11,36 @@ import view.AnimatorVisualView;
 
 public class Main {
   public static void main(String[] args) {
-    IAnimatorModelState<AShape> model = new SimpleAnimatorModel.TweenBuilder()
-            .addRectangle("1", 10,10,100,100,106,110,120,0,100)
-            .addColorChange("1", 10,10,10,15,15,15,0,5)
-            .addMove("1", 10,10,15,40, 6, 7)
-            .addMove("1", 15,40,10,10, 8, 15)
-            .addScaleToChange("1", 100, 100, 200, 200,15, 30)
-            .addOval("2", 10,10,10,50, 200, 200,200, 0,50)
-            .addMove("2", 10,10,50,50,0,10)
-            .build();
-    AnimatorVisualView view = new AnimatorVisualView(0, model.getShapes());
-
-    view.makeVisible();
-
-    Timer t = new Timer(30, new ActionListener() {
-      @Override
-      public void actionPerformed(ActionEvent ae) {
-
-        int tick = 0;
-        view.refresh();
-        view.makeVisible();
-        System.out.println(model.getShapeAtTick(tick,"1").getPosition().getX());
-        System.out.println("hi");
-        tick++;
-      };
-
-    });
-    t.start();
-
-
-    view.makeVisible();
+//    IAnimatorModelState<AShape> model = new SimpleAnimatorModel.TweenBuilder()
+//            .addRectangle("1", 10,10,100,100,106,110,120,0,100)
+//            .addColorChange("1", 10,10,10,15,15,15,0,5)
+//            .addMove("1", 10,10,15,40, 6, 7)
+//            .addMove("1", 15,40,10,10, 8, 15)
+//            .addScaleToChange("1", 100, 100, 200, 200,15, 30)
+//            .addOval("2", 10,10,10,50, 200, 200,200, 0,50)
+//            .addMove("2", 10,10,50,50,0,10)
+//            .build();
+//    AnimatorVisualView view = new AnimatorVisualView(0, model.getShapes());
+//
+//    view.makeVisible();
+//
+//    Timer t = new Timer(30, new ActionListener() {
+//      @Override
+//      public void actionPerformed(ActionEvent ae) {
+//
+//        int tick = 0;
+//        view.refresh();
+//        view.makeVisible();
+//        System.out.println(model.getShapeAtTick(tick,"1").getPosition().getX());
+//        System.out.println("hi");
+//        tick++;
+//      };
+//
+//    });
+//    t.start();
+//
+//
+//    view.makeVisible();
 
   }
 }
