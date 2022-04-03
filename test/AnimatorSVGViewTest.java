e6b459af4ed5d63f9a54ebd5362b64980a3f20ba
BillQK <billngo47@gmail.com>
2022-04-03 16:12:29 -0400

    push

--- test/AnimatorSVGViewTest.java
+++ test/AnimatorSVGViewTest.java
@@ -2,6 +2,10 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import model.IAnimatorModelState;
 import model.SimpleAnimatorModel;
@@ -9,7 +13,9 @@ import model.shape.AShape;
 import view.AnimatorSVGView;
 import view.IAnimatorView;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 public class AnimatorSVGViewTest {
   IAnimatorModelState<AShape> model;
@@ -18,36 +24,37 @@ public class AnimatorSVGViewTest {
   @Before
   public void setUp() {
     model = new SimpleAnimatorModel.TweenBuilder()
-            .addRectangle("1", 10,10,100,100,10,10,10,0,100)
-            .addColorChange("1", 10,10,10,15,15,15,0,5)
-            .addMove("1", 10,10,15,40, 6, 7)
-            .addMove("1", 15,40,10,10, 8, 15)
-            .addScaleToChange("1", 100, 100, 200, 200,15, 30)
-            .addOval("2", 10,10,10,50, 10, 10,10, 0,50)
-            .addMove("2", 10,10,50,50,0,10)
+            .addRectangle("1", 10, 10, 100, 100, 10, 10, 10, 0, 100)
+            .addColorChange("1", 10, 10, 10, 15, 15, 15, 0, 5)
+            .addMove("1", 10, 10, 15, 40, 6, 7)
+            .addMove("1", 15, 40, 10, 10, 8, 15)
+            .addScaleToChange("1", 100, 100, 200, 200, 15, 30)
+            .addOval("2", 10, 10, 10, 50, 10, 10, 10, 0, 50)
+            .addMove("2", 10, 10, 50, 50, 0, 10)
             .build();
 
     view = new AnimatorSVGView(model, 10);
   }
+
   @Test
   public void testGetDetails() {
     assertEquals(view.getDetails(),
             "<svg width=\"700\" height=\"500\" version=\"1.1\"\n" +
-            "     xmlns=\"http://www.w3.org/2000/svg\">\n" +
-            "<rect id=\"1\" x=\"10.0\" y=\"10.0\" width=\"100.0\" height=\"100.0\" fill=\"rgb(10,10,10)\" visibility=\"visible\" >\n" +
-            "<animate attributeType=\"xml\" begin=\"0.0ms\" dur=\"500.0ms\" attributeName=\"rgb\" from=\"(10,10,10)\" to=\"(15,15,15)\" fill=\"freeze\" /> \n" +
-            "<animate attributeType=\"xml\" begin=\"600.0ms\" dur=\"100.0ms\" attributeName=\"x\" from=\"10.0\" to=\"15.0\" fill=\"freeze\" /> \n" +
-            "<animate attributeType=\"xml\" begin=\"600.0ms\" dur=\"100.0ms\" attributeName=\"y\" from=\"10.0\" to=\"40.0\" fill=\"freeze\" />\n" +
-            "<animate attributeType=\"xml\" begin=\"800.0ms\" dur=\"700.0ms\" attributeName=\"x\" from=\"10.0\" to=\"10.0\" fill=\"freeze\" /> \n" +
-            "<animate attributeType=\"xml\" begin=\"800.0ms\" dur=\"700.0ms\" attributeName=\"y\" from=\"10.0\" to=\"10.0\" fill=\"freeze\" />\n" +
-            "<animate attributeType=\"xml\" begin=\"1500.0ms\" dur=\"1500.0ms\" attributeName=\"width\" from=\"100.0\" to=\"200.0\" fill=\"freeze\" /> \n" +
-            "<animate attributeType=\"xml\" begin=\"1500.0ms\" dur=\"1500.0ms\" attributeName=\"height\" from=\"100.0\" to=\"200.0\" fill=\"freeze\" />\n" +
-            "</rect>\n" +
-            "<ellipse id=\"2\" cx=\"10.0\" cy=\"10.0\" rx=\"10.0\" ry=\"50.0\" fill=\"rgb(10,10,10)\" visibility=\"visible\" >\n" +
-            "<animate attributeType=\"xml\" begin=\"0.0ms\" dur=\"1000.0ms\" attributeName=\"cx\" from=\"10.0\" to=\"50.0\" fill=\"freeze\" /> \n" +
-            "<animate attributeType=\"xml\" begin=\"0.0ms\" dur=\"1000.0ms\" attributeName=\"cy\" from=\"10.0\" to=\"50.0\" fill=\"freeze\" />\n" +
-            "</ellipse>\n" +
-            "</svg>");
+                    "     xmlns=\"http://www.w3.org/2000/svg\">\n" +
+                    "<rect id=\"1\" x=\"10.0\" y=\"10.0\" width=\"100.0\" height=\"100.0\" fill=\"rgb(10,10,10)\" visibility=\"visible\" >\n" +
+                    "<animate attributeType=\"xml\" begin=\"0.0ms\" dur=\"500.0ms\" attributeName=\"rgb\" from=\"(10,10,10)\" to=\"(15,15,15)\" fill=\"freeze\" /> \n" +
+                    "<animate attributeType=\"xml\" begin=\"600.0ms\" dur=\"100.0ms\" attributeName=\"x\" from=\"10.0\" to=\"15.0\" fill=\"freeze\" /> \n" +
+                    "<animate attributeType=\"xml\" begin=\"600.0ms\" dur=\"100.0ms\" attributeName=\"y\" from=\"10.0\" to=\"40.0\" fill=\"freeze\" />\n" +
+                    "<animate attributeType=\"xml\" begin=\"800.0ms\" dur=\"700.0ms\" attributeName=\"x\" from=\"10.0\" to=\"10.0\" fill=\"freeze\" /> \n" +
+                    "<animate attributeType=\"xml\" begin=\"800.0ms\" dur=\"700.0ms\" attributeName=\"y\" from=\"10.0\" to=\"10.0\" fill=\"freeze\" />\n" +
+                    "<animate attributeType=\"xml\" begin=\"1500.0ms\" dur=\"1500.0ms\" attributeName=\"width\" from=\"100.0\" to=\"200.0\" fill=\"freeze\" /> \n" +
+                    "<animate attributeType=\"xml\" begin=\"1500.0ms\" dur=\"1500.0ms\" attributeName=\"height\" from=\"100.0\" to=\"200.0\" fill=\"freeze\" />\n" +
+                    "</rect>\n" +
+                    "<ellipse id=\"2\" cx=\"10.0\" cy=\"10.0\" rx=\"10.0\" ry=\"50.0\" fill=\"rgb(10,10,10)\" visibility=\"visible\" >\n" +
+                    "<animate attributeType=\"xml\" begin=\"0.0ms\" dur=\"1000.0ms\" attributeName=\"cx\" from=\"10.0\" to=\"50.0\" fill=\"freeze\" /> \n" +
+                    "<animate attributeType=\"xml\" begin=\"0.0ms\" dur=\"1000.0ms\" attributeName=\"cy\" from=\"10.0\" to=\"50.0\" fill=\"freeze\" />\n" +
+                    "</ellipse>\n" +
+                    "</svg>");
   }
 
   @Test
@@ -55,5 +62,19 @@ public class AnimatorSVGViewTest {
     view.writeFile("test");
     File file = new File("test.svg");
     assertTrue(file.exists());
+  }
+
+
+  @Test
+  public void testWriteFileContents() {
+    view.writeFile("test");
+    File file = new File("test.svg");
+    String content = "";
+    try {
+      content = Files.readString(Path.of("test.svg"), StandardCharsets.US_ASCII);
+    } catch (IOException e) {
+      fail();
+    }
+    assertEquals(view.getDetails(), content);
   }
 }
---
+++ test/ICommandsStateTest.java
@@ -0,0 +1,74 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ICommandsStateTest {
+
+  @Test
+  public void getBeginsState() {
+  }
+
+  @Test
+  public void getEndsState() {
+  }
+
+  @Test
+  public void getState() {
+  }
+
+  @Test
+  public void getTheShape() {
+  }
+
+  @Test
+  public void getType() {
+  }
+
+  @Test
+  public void getStart() {
+  }
+
+  @Test
+  public void getEnd() {
+  }
+
+  @Test
+  public void getOldPosn() {
+  }
+
+  @Test
+  public void getNewPosn() {
+  }
+
+  @Test
+  public void getOldWidth() {
+  }
+
+  @Test
+  public void getNewWidth() {
+  }
+
+  @Test
+  public void getOldHeight() {
+  }
+
+  @Test
+  public void getNewHeight() {
+  }
+
+  @Test
+  public void getOldColor() {
+  }
+
+  @Test
+  public void getNewColor() {
+  }
+
+  @Test
+  public void getShapeAtTick() {
+  }
+
+  @Test
+  public void getSVG() {
+  }
+}
