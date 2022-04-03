989b0c816f3493bc4f620c8c4fa8de9df39c7d20
BillQK <billngo47@gmail.com>
2022-04-03 17:32:51 -0400

    add something

---
+++ src/view/AnimatorViewCreator.java
@@ -0,0 +1,35 @@
+package view;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+import model.IAnimatorModelState;
+import model.shape.AShape;
+
+/**
+ * A factory representation of a AnimotorViewCreator,
+ * Its usage is to return a instance of Animator View object.
+ */
+public class AnimatorViewCreator {
+  private static final double tempo = 10;
+
+  /**
+   * A method that return an instance of the given View.
+   * @param view a String
+   * @param model a IAnimatorModelState model
+   * @return IAnimatorView
+   */
+  public static IAnimatorView create(String view, IAnimatorModelState<AShape> model) {
+    if (Objects.equals(view, "")) {
+      throw new IllegalArgumentException("Type of view cannot be empty");
+    } else if (Objects.equals(view, "svg")) {
+      return new AnimatorSVGView(model, tempo);
+    } else if (Objects.equals(view, "text")) {
+      return new AnimatorTextView(model);
+    } else if (Objects.equals(view, "visual")) {
+      return new AnimatorVisualView(tempo, new ArrayList<>(), model);
+    } else {
+      throw new IllegalArgumentException("Invalid view type");
+    }
+  }
+}
