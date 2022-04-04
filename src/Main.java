a6d64a4e5a331909b5674f0a1367963b982fde08
Khanh Nguyen <khanhnguyen@Khanhs-MacBook-Pro.local>
2022-04-03 22:35:01 -0400

    Create class time of your model state

--- src/Main.java
+++ src/Main.java
@@ -17,13 +17,20 @@ import view.IAnimatorView;
 public class Main {
   public static void main(String[] args) {
     IAnimatorModelState<AShape> model = new SimpleAnimatorModel.TweenBuilder()
-            .addRectangle("1", 10,10,100,100,106,110,120,0,100)
-            .addColorChange("1", 10,10,10,15,15,15,0,5)
-            .addMove("1", 10,10,15,40, 6, 7)
-            .addMove("1", 15,40,10,10, 8, 15)
-            .addScaleToChange("1", 100, 100, 200, 200,15, 30)
-            .addOval("2", 10,10,10,50, 200, 200,200, 0,50)
-            .addMove("2", 10,10,50,50,0,10)
+            .addRectangle("1", 10,10,100,100,
+                    106,110,120,0,40)
+            .addColorChange("1", 10,10,10,
+                    15,15,15,0,5)
+            .addMove("1", 10,10,
+                    15,40, 6, 7)
+            .addMove("1", 15,40,
+                    10,10, 8, 15)
+            .addScaleToChange("1", 100, 100,
+                    200, 200,15, 30)
+            .addOval("2", 10,10,10,50,
+                    200, 200,200, 0,50)
+            .addMove("2", 10,10,
+                    50,50,0,10)
             .build();
     AnimatorVisualView view = new AnimatorVisualView(0, model.getShapes(), model);
 
@@ -33,14 +40,14 @@ public class Main {
       @Override
       public void actionPerformed(ActionEvent ae) {
 
-        int tick = 30;
-        view.refresh();
-        view.makeVisible();
-        for (int i = 0; i < tick; i++) {
-          System.out.println(model.getShapeAtTick(i, "1").getPosition().getX());
-        }
-        System.out.println("hi");
-        tick++;
+//        int tick = 30;
+//        view.refresh();
+//        view.makeVisible();
+//        for (int i = 0; i < tick; i++) {
+//          System.out.println(model.getShapeAtTick(i, "1").getPosition().getX());
+//        }
+//        System.out.println("hi");
+//        tick++;
       };
 
     });
@@ -67,6 +74,18 @@ public class Main {
 
     } catch (IOException e) {
       e.printStackTrace();
+    }
+  }
+
+  public static class Tempo {
+    double tempo = 0;
+
+    public double getTempo() {
+      return this.tempo;
+    }
+
+    public void addTempo() {
+      tempo++;
     }
   }
 }
