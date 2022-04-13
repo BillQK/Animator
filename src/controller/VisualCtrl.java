f6f0b6fd7146b017b029534dbce3afaf1b4dfe3e
Khanh Nguyen <khanhnguyen@Khanhs-MacBook-Pro.local>
2022-04-12 21:43:17 -0400

    Controller

--- src/controller/VisualCtrl.java
+++ src/controller/VisualCtrl.java
@@ -1,24 +1,42 @@
 package controller;
 
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
 import model.IAnimatorModel;
+import model.command.ACommand;
+import model.utils.Tempo;
 import view.IAnimatorView;
 
 public class VisualCtrl implements IAnimatorController {
   private IAnimatorModel model;
   private IAnimatorView view;
   private double tempo;
+  private Tempo t;
+  private Timer timer;
 
   VisualCtrl(IAnimatorModel model, IAnimatorView view, double tempo) {
     this.model = model;
     this.view = view;
     this.tempo = tempo;
+    this.t = null;
+    this.timer = null;
   }
 
   @Override
   public void start() {
-    long starttime = System.currentTimeMillis();
+    this.t = new Tempo(this.tempo);
+
+    IAnimatorModel finalmodel = model;
+    IAnimatorView finalview = view;
+
+    view.makeVisible();
+
+    ActionListener timeListner = ae -> {
+
+    }
+
   }
 
   @Override
